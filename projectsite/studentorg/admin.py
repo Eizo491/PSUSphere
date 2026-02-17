04a100cc2306fc6bb31cf1da13e282f4a7ada23b
Eizo <me@example.com>
2026-02-17 23:37:51 +0800

    loaded initial data

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "updated_at")
+    search_fields = ("college_name",)
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college", "updated_at")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "program", "updated_at")
+    search_fields = ("lastname", "firstname", "student_id")
+    list_filter = ("program",)
